b512025226c90130cdb5afa5f0de059d297ab53f
Rugwiro Mbdanda Valentin <rugwirobaker@gmail.com>
2019-06-01 12:38:30 +0200

    [NOT CLEAN] Refactor unit test structure
    moved testcases into a test table for better structure and visibility.

--- app/auth_test.go
+++ app/auth_test.go
@@ -6,68 +6,87 @@ import (
 )
 
 func TestGetAuthToken(t *testing.T){
-	ctx:= context.Background()
-
 	app:= &Application{}
-	req:= &GetTokenReq{
-		ID: "1",
-	}
 
-	res, err:= app.GetAuthToken(&ctx, req)
-	if err!=nil{
-		t.Errorf("expected error to be nil")
-	}
-
-	//request and response must have the same id.
-	if res.ID != req.ID{
-		t.Errorf("expected response and request to have the same id found-> req:%s | res:%s", req.ID, res.ID)
+	testcases:= []struct{
+		request 	*GetTokenReq
+		err 		error
+	}{
+		{request: &GetTokenReq{ID: "1"}, err:nil},
 	} 
-	
-	//the response must have a token field
-	if len(res.Token)< 1{
-		t.Errorf("the token field must not be empty")
+	for _,tc:=range testcases{
+		ctx:= context.Background()
+		res, err:= app.GetAuthToken(&ctx, tc.request)
+
+		if err!=tc.err{
+			t.Errorf("expected error to be nil")
+		}
+
+		//request and response must have the same id.
+		if res.ID != tc.request.ID{
+			t.Errorf("expected response and request to have the same id found-> req:%s | res:%s", tc.request.ID, res.ID)
+		} 
+
+		//the response must have a token field
+		if len(res.Token)< 1{
+			t.Errorf("the token field must not be empty")
+		}
 	}
 }
 
 func TestRenewAuthToken(t *testing.T){
-	ctx:= context.Background()
-
 	app:= &Application{}
-	req:= &RenewTokenReq{
-		ID: "10",
+
+	testcases:= []struct{
+		request *RenewTokenReq
+		err     error
+	}{
+		{request: &RenewTokenReq{ID: "1"}, err:nil},
 	}
 
-	res, err:= app.RenewAuthToken(&ctx, req)
-	if err!=nil{
-		t.Errorf("expected error to be nil")
-	}
-	
-	//request and response must have the same id.
-	if res.ID != req.ID{
-		t.Errorf("expected response and request to have the same id found-> req:%s | res:%s", req.ID, res.ID)
-	}
+	for _,tc:= range testcases{
+		ctx:= context.Background()
 
-	//the response must have a token field
-	if len(res.Token)< 1{
-		t.Errorf("the token field must not be empty")
+		res, err:= app.RenewAuthToken(&ctx, tc.request)
+
+		if err!=tc.err{
+			t.Errorf("expected error to be nil")
+		}
+
+		//request and response must have the same id.
+		if res.ID != tc.request.ID{
+			t.Errorf("expected response and request to have the same id found-> req:%s | res:%s", tc.request.ID, res.ID)
+		}
+
+		//the response must have a token field
+		if len(res.Token)< 1{
+			t.Errorf("the token field must not be empty")
+		}
 	}
 }
 
 func TestRevokeAuthToken(t *testing.T){
-	ctx:= context.Background()
-
 	app:= &Application{}
-	req:= &RevokeTokenReq{
-		ID: "10",
+
+	testcases:= []struct{
+		request *RevokeTokenReq
+		err     error
+	}{
+		{request: &RevokeTokenReq{ID: "1"}, err:nil},
 	}
 
-	res,err:= app.RevokeAuthToken(&ctx, req)
-	if err!=nil{
-		t.Errorf("expected error to be nil")
-	}
+	for _,tc:= range testcases{
+		ctx:= context.Background()
 
-	//request and response must have the same id.
-	if res.ID != req.ID{
-		t.Errorf("expected response and request to have the same id found-> req:%s | res:%s", req.ID, res.ID)
-	} 
+		res,err:= app.RevokeAuthToken(&ctx, tc.request)
+		
+		if err!=nil{
+			t.Errorf("expected error to be nil")
+		}
+
+		//request and response must have the same id.
+		if res.ID != tc.request.ID{
+			t.Errorf("expected response and request to have the same id found-> req:%s | res:%s", tc.request.ID, res.ID)
+		} 
+	}
 }
