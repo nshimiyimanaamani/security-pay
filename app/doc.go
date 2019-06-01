37ed09b9cb500d1649427e2825fd97fc67759b5d
Rugwiro Mbdanda Valentin <rugwirobaker@gmail.com>
2019-06-01 15:14:32 +0200

    Add app package documentation.

---
+++ app/doc.go
@@ -0,0 +1,4 @@
+package app
+
+//package app contains the application business logic and the corresponding state.
+//it contains the different service interfaces and their implementation onto the root App type.
