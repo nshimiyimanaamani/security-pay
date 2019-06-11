587ab206eb13c6ed0c9126fc2c36594240b1d35b
Rugwiro Mbdanda Valentin <rugwirobaker@gmail.com>
2019-06-11 13:56:45 +0200

    [WIP]Test the todo probot

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+/**
+ * @todo Invite fred
+ * @body This is just a test to check on the tdo
+ */
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
