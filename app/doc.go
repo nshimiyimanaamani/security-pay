0ae3a9f14085ce93c7b74ec1173a1cce48536d6b
Rugwiro Mbdanda Valentin <rugwirobaker@gmail.com>
2019-06-11 14:07:27 +0200

    [TODO]Paymeny History Service

--- app/doc.go
+++ app/doc.go
@@ -2,3 +2,8 @@ package app
 
 //package app contains the application business logic and the corresponding state.
 //it contains the different service interfaces and their implementation onto the root App type.
+
+/**
+ * @todo Add Payment History Interface
+ * @body The payment History interfaces deals with listing all the payment receipts.
+ */
