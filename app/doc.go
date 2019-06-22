e7183588959e384d4cc261a0941c520a2bdce0c6
Rugwiro Mbdanda Valentin <rugwirobaker@gmail.com>
2019-06-22 20:21:25 +0200

    [MINOR]Update todo

--- app/doc.go
+++ app/doc.go
@@ -2,8 +2,3 @@ package app
 
 //package app contains the application business logic and the corresponding state.
 //it contains the different service interfaces and their implementation onto the root App type.
-
-/**
- * @todo Add Transaction History Interface
- * @body The transaction history interface deals with listing all the payment receipts.
- */
