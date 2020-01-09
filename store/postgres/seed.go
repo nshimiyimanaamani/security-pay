21099374124971bc6a5d6f1537f1f0bda5acb73e
Shema-Fred <rugwirobaker@gmail.com>
2020-01-09 05:16:39 +0200

    Add village and cell relations

--- store/postgres/seed.go
+++ store/postgres/seed.go
@@ -13,6 +13,9 @@ func seedDB(db *sql.DB) error {
 		INSERT INTO sectors (sector) VALUES('paypack.developers') ON CONFLICT (sector) do nothing;
 		INSERT INTO sectors (sector) VALUES('gasabo.kimironko') ON CONFLICT (sector) do nothing;
 		INSERT INTO sectors (sector) VALUES('gasabo.remera') ON CONFLICT (sector) do nothing;
+
+		INSERT INTO cells(cell, sector) VALUES('cell', 'paypack.test') ON CONFLICT (cell) do nothing;
+		INSERT INTO villages(village, cell, sector) VALUES('village', 'cell', 'paypack.test') ON CONFLICT (village) do nothing;
 	`
 	_, err := db.Exec(seed)
 	if err != nil {
