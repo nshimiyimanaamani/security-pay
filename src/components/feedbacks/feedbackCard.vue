1271783a354e45c97ad5301760d20f4fb9d07658
shema-fred <shemafred3@gmail.com>
2020-01-12 01:06:30 +0200

    added: feedbacks

---
+++ src/components/feedbacks/feedbackCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-card
+    :title="feedback.title"
+    :sub-title="'By '+feedback.display_name+' ['+feedback.creator+']'"
+    class="feedback-card my-2"
+  >
+    <hr />
+    <b-card-text>{{feedback.body}}</b-card-text>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "feedback-card",
+  props: {
+    feedback: Object
+  },
+  data() {
+    return {
+      state: {
+        loading: false
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.feedback-card {
+  text-transform: capitalize;
+}
+.feedback-card .card-title {
+  font-size: 20px;
+}
+.feedback-card .card-subtitle {
+  text-transform: uppercase;
+  font-size: 13px;
+}
+.feedback-card .card-text {
+  font-size: 15px;
+}
+</style>
--- src/pages/feedbacks.vue
+++ src/pages/feedbacks.vue
@@ -3,10 +3,18 @@
     <vue-title title="Paypack | Feedbacks" />
     <b-card no-body class="nav-controls">
       <b-tabs pills card vertical lazy>
-        <b-tab title="FeedBacks">hello you are going to list all feedbacks</b-tab>
-        <b-tab title="Send FeedBack" active>
-          <add-feedback />
+        <b-tab title="FeedBacks" active @click="loadData">
+          <div v-for="(feedback,index) in feedbacks" :key="index">
+            <feedback :feedback="feedback" v-if="!state.loading" />
+          </div>
+          <div v-if="state.loading" class="text-center m-5">
+            <b-spinner small></b-spinner>
+            <strong>Loading...</strong>
+          </div>
         </b-tab>
+        <!-- <b-tab title="Send FeedBack">
+          <add-feedback />
+        </b-tab> -->
       </b-tabs>
     </b-card>
   </b-container>
@@ -14,10 +22,50 @@
 
 <script>
 import addFeedback from "../components/feedbacks/addFeedback.vue";
+import feedbackCard from "../components/feedbacks/feedbackCard.vue";
 export default {
   name: "feedbacks",
   components: {
-    "add-feedback": addFeedback
+    "add-feedback": addFeedback,
+    feedback: feedbackCard
+  },
+  data() {
+    return {
+      state: {
+        loading: false
+      },
+      feedbacks: null
+    };
+  },
+  computed: {
+    endpoint() {
+      return this.$store.getters.getEndpoint;
+    }
+  },
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    loadData() {
+      this.state.loading = true;
+      this.axios
+        .get(this.endpoint + "/feedback?offset=0&limit=10")
+        .then(res => {
+          this.state.loading = false;
+          this.feedbacks = res.data.Messages;
+        })
+        .catch(err => {
+          if (navigator.onLine) {
+            const error = isNullOrUndefined(err.response)
+              ? "an error occured"
+              : err.response.data.error || err.response.data;
+            this.$snotify.error(error);
+          } else {
+            this.$snotify.error("Please connect to the internet");
+          }
+          this.state.loading = false;
+        });
+    }
   }
 };
 </script>
