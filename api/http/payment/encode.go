fc57a82df129d8c361a1f77ec42a9a5f4e1828ed
Shema-Fred <rugwirobaker@gmail.com>
2020-01-13 15:26:23 +0200

    Patch payment module
    * update the callback response to better treat empty fields.
    * add missing field callbackdata.ChanneRef.
    * improve payload decoding.
    * improve error handling at layer one(http).

--- api/http/payment/encode.go
+++ api/http/payment/encode.go
@@ -2,35 +2,15 @@ package payment
 
 import (
 	"encoding/json"
-	"strings"
 
-	"io"
 	"net/http"
-
-	"github.com/rugwirobaker/paypack-backend/pkg/errors"
 )
 
-var (
-	contentType = "application/json"
-)
+func encode(w http.ResponseWriter, code int, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
 
-// VerifyContentType middleware checks content typ
-func VerifyContentType(r *http.Request) error {
-	const op errors.Op = ""
-	if !strings.Contains(r.Header.Get("Content-Type"), contentType) {
-		return errors.E(op, "unsupported content type", errors.KindBadRequest)
-	}
-	return nil
-}
-
-func encodeRes(w io.Writer, i interface{}) error {
-	if headered, ok := w.(http.ResponseWriter); ok {
-		headered.Header().Set("Cache-Control", "no-cache")
-		headered.Header().Set("Content-type", "application/json")
-
-		return json.NewEncoder(w).Encode(i)
-	}
-	return nil
+	w.WriteHeader(code)
+	return json.NewEncoder(w).Encode(response)
 }
 
 func encodeErr(w http.ResponseWriter, code int, err error) {
