9e12f4127168f09734b20613ccdf65fa27348b1a
Rugwiro Valentin <rugwirobaker@users.noreply.github.com>
2020-02-09 23:23:22 +0200

    [PATCH] Add delete to allowed cors methods

--- cmd/paypack/app/app.go
+++ cmd/paypack/app/app.go
@@ -59,7 +59,7 @@ func Bootstrap(conf *config.Config) (http.Handler, error) {
 
 	cors := handlers.CORS(
 		handlers.AllowedOrigins([]string{"*"}),
-		handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"}),
+		handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"}),
 		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
 	)
 	return cors(r), nil
