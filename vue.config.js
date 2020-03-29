01d56d16c260e3dafa7154d6d7bd238eeb1efb2d
shema-fred <shemafred3@gmail.com>
2020-03-29 02:35:57 +0200

    reduce loading time

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-    productionSourceMap: false
+  productionSourceMap: false,
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: "all",
+            maxSize: 250000,
+            maxAsyncRequests: 250000,
+            maxInitialRequests: 250000,
+            reuseExistingChunk: true,
+            enforce: true,
+            priority: 1
+          }
+        }
+      }
+    }
+  }
 };
