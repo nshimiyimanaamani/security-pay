343bf0dd8d1a8129653b522815b0e165158237ab
Rugwiro Valentin <rugwirobaker@gmail.com>
2020-06-01 12:05:25 +0200

    Fix owner validation

--- core/owners/owners_test.go
+++ core/owners/owners_test.go
@@ -16,22 +16,27 @@ func TestValidateOwner(t *testing.T) {
 	}{
 		{
 			desc:  "validate with a valid owner entity",
-			owner: Owner{Fname: "James", Lname: "Torredo", Phone: "0784677882"},
+			owner: Owner{Fname: "James", Lname: "Torredo", Phone: "0784677882", Namespace: "namespace"},
 			err:   nil,
 		},
 		{
 			desc:  "validate with empty fname field",
-			owner: Owner{Lname: "Torredo", Phone: "0784677882"},
+			owner: Owner{Lname: "Torredo", Phone: "0784677882", Namespace: "namespace"},
 			err:   ErrInvalidEntity,
 		},
 		{
 			desc:  "validate with empty lname field",
-			owner: Owner{Fname: "James", Phone: "0784677882"},
+			owner: Owner{Fname: "James", Phone: "0784677882", Namespace: "namespace"},
 			err:   ErrInvalidEntity,
 		},
 		{
 			desc:  "validate with invalid phone number",
-			owner: Owner{Fname: "James", Lname: "Torredo", Phone: "77878333"},
+			owner: Owner{Fname: "James", Lname: "Torredo", Phone: "77878333", Namespace: "namespace"},
+			err:   ErrInvalidEntity,
+		},
+		{
+			desc:  "validate with invalid namespace",
+			owner: Owner{Fname: "James", Lname: "Torredo", Phone: "0784677882"},
 			err:   ErrInvalidEntity,
 		},
 	}
